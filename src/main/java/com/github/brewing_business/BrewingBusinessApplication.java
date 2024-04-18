86d2b662778cbe794126a183d62d3ed1b07958e8
Hi <iamjsjs0319@gmail.com>
2024-04-18 22:37:42 +0900

    init
    - 초기파일

---
+++ src/main/java/com/github/brewing_business/BrewingBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.github.brewing_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewingBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewingBusinessApplication.class, args);
+    }
+
+}
