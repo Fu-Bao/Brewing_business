654c6c6e8a7938b77e4e15bb76359da91c745210
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-04-18 22:30:22 +0900

    -init

---
+++ src/test/java/com/github/brewing_business/BrewingBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brewing_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
